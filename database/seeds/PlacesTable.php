2f4505e413fc2f8ce3068485e1d0f442358c7858
xaviergallezot <xavier.gallezot@hotmail.fr>
2019-03-25 08:29:41 -0700

    Fin de databases et seeder

--- database/seeds/PlacesTable.php
+++ database/seeds/PlacesTable.php
@@ -11,8 +11,45 @@ class PlacesTable extends Seeder
      */
     public function run()
     {
-        factory(App/User::class, 50)->create()->each(function ($user){
-          $user->posts()
-        })
+        DB::table('places')->insert(
+          array(
+            array(
+            'id'=>1,
+            'Dispo_place'=>TRUE,
+            'created_at'=>'2019-12-12 08:00:00',
+            'updated_at'=>'2019-12-12 08:00:00'
+          ),
+
+            array(
+              'id'=>2,
+            'Dispo_place'=>TRUE,
+            'created_at'=>'2019-12-12 01:00:00',
+            'updated_at'=>'2019-12-12 01:00:00'
+          ),
+            array(
+              'id'=>3,
+            'Dispo_place'=>TRUE,
+            'created_at'=>'2019-12-12 01:00:00',
+            'updated_at'=>'2019-12-12 01:00:00'
+          ),
+            array(
+              'id'=>4,
+            'Dispo_place'=>TRUE,
+            'created_at'=>'2019-12-12 01:00:00',
+            'updated_at'=>'2019-12-12 01:00:00'
+          ),
+            array(
+              'id'=>5,
+            'Dispo_place'=>TRUE,
+            'created_at'=>'2019-12-12 01:00:00',
+            'updated_at'=>'2019-12-12 01:00:00'
+          )
+)
+
+);
+
+
+
+
     }
 }
